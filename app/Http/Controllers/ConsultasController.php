62e2c5b50971c48f3275ae27fcc9eeb8d641337a
Lucas Randisi <lrandisi@kodear.net>
2022-05-08 21:20:10 -0300

    Listar consultas filtradas por estudiante u horario de consulta, ordenamineto por fecha de creación

--- app/Http/Controllers/ConsultasController.php
+++ app/Http/Controllers/ConsultasController.php
@@ -8,7 +8,25 @@ use Illuminate\Http\Request;
 class ConsultasController extends Controller
 {
     public function index(Request $request) {
-		return Consulta::all();
+    	$filters = $request->input('filters', []);
+    	$orders = $request->input('orders', []);
+
+		$query = Consulta::query();
+
+		foreach ($filters as $key => $value) {
+			if (in_array($key, ['estudiante_id', 'horario_consulta_id'])) {
+				$query->where($key, '=', $value);
+			}
+		}
+
+
+		foreach ($orders as $key => $value) {
+			if (in_array($key, ['created_at'])) {
+				$query->orderBy($key, $value);
+			}
+		}
+
+		return $query->get();
 	}
 
 	public function create() {
