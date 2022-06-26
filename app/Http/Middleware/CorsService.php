ce424c5a5c362333b4778767ab05b4f3702a8658
Lucas Randisi <lucasrandisi@gmail.com>
2022-06-26 02:33:51 -0300

    Cors

--- app/Http/Middleware/CorsService.php
+++ app/Http/Middleware/CorsService.php
@@ -177,9 +177,7 @@ class CorsService
 	{
 		if ($this->allowAllOrigins === true && !$this->supportsCredentials) {
 			// Safe+cacheable, allow everything
-			if (config('app.env') !== 'production') {
-				$response->headers->set('Access-Control-Allow-Origin', '*');
-			}
+			$response->headers->set('Access-Control-Allow-Origin', '*');
 		} elseif ($this->isSingleOriginAllowed()) {
 			// Single origins can be safely set
 			$response->headers->set('Access-Control-Allow-Origin', array_values($this->allowedOrigins)[0]);
